27e377f4b34abc8cd9fe7805c6a2b1c5090e90d8
FailureMaster <mike.macabulos@gmail.com>
2025-07-25 07:52:39 +0000

    Added/User role in users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name')->nullable();
             $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->string('user_role')->unique();
             $table->string('phone');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
